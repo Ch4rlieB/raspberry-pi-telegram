5615238e11376643afe69cf5cdc008297e174c4b
ch4rlieb <karel.bel@dativery.com>
2019-05-15 16:09:05 +0200

    support for sending photos from camera

--- telegram.py
+++ telegram.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import time
 import telepot
+import picamera
 from telepot.loop import MessageLoop
 
 # add --on-boot param if script is started during raspberry pi booting
@@ -17,6 +18,7 @@ if onboot:
 
 userId = 1234567890 # chat.id from message, only we can control our Raspberry Pi
 exited = False
+picturePath = '/path/to/photo.jpg'
 
 bot = telepot.Bot('API TOKEN')
 
@@ -38,10 +40,22 @@ def handleMsg(msg):
       command = msg['text']
       if command == '/help':
         output = output + '/help - prints this help\n'
+        output = output + '/photo - sends actual photo from camera\n'
         output = output + '/uptime - shows uptime from RPi\n'
         output = output + '/df - shows free disk space\n'
         output = output + '/die - stops bot\n'
         output = output + '/reboot - reboot RPi\n'
+      elif command == '/photo':
+        camera = picamera.PiCamera()
+        try:
+          time.sleep(5)
+          camera.resolution = '1080p'
+          camera.hflip = True
+          camera.vflip = True
+          camera.capture(picturePath)
+          bot.sendPhoto(chatId, open(picturePath, 'rb'))
+        finally:
+          camera.close()
       elif command == '/uptime':
         output = subprocess.check_output('uptime')
       elif command == '/df':
